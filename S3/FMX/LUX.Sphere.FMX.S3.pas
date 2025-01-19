afaeaabf85fc9e09a31c55d9799174f9dd1ea7e3
luxidea <contact@luxidea.net>
2025-01-19 23:32:39 +0900

    LUX.S3.FMX → LUX.Sphere.FMX.S3 ユニットに改名。

--- S3/FMX/LUX.Sphere.FMX.S3.pas
+++ S3/FMX/LUX.Sphere.FMX.S3.pas
@@ -1,4 +1,4 @@
-﻿unit LUX.S3.FMX;
+﻿unit LUX.Sphere.FMX.S3;
 
 interface //#################################################################### ■
 
@@ -8,72 +8,12 @@ uses System.Types, System.SysUtils, System.RTLConsts, System.Classes,
      LUX, LUX.D3, LUX.Quaternion, LUX.D4x4,
      LUX.FMX.Graphics.D3,
      LUX.FMX.Graphics.D3.Shaper,
+     LUX.Sphere.FMX,
      LIB.Poins.S3;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
-     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
-
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TWorld3D
-
-     TWorld3D = class( TF3DWorld )
-     private
-     protected
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCamera3D
-
-     TCamera3D = class( TF3DCamera )
-     private
-     protected
-       _Radius :Single;
-       _AngleX :Single;
-       _AngleY :Single;
-       ///// A C C E S S O R
-       function GetRadius :Single;
-       procedure SetRadius( const Radius_:Single );
-       function GetAngleX :Single;
-       procedure SetAngleX( const AngleX_:Single );
-       function GetAngleY :Single;
-       procedure SetAngleY( const AngleY_:Single );
-       ///// M E T H O D
-       procedure MakePose;
-     public
-       constructor Create( Owner_:TComponent ); override;
-       destructor Destroy; override;
-       ///// P R O P E R T Y
-       property Radius :Single read GetRadius write SetRadius;
-       property AngleX :Single read GetAngleX write SetAngleX;
-       property AngleY :Single read GetAngleY write SetAngleY;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TLight3D
-
-     TLight3D = class( TF3DLight )
-     private
-     protected
-     public
-       constructor Create( Owner_:TComponent ); override;
-       destructor Destroy; override;
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSphere3D
-
-     TSphere3D = class( TF3DSphere )
-     private
-     protected
-       ///// A C C E S S O R
-       function GetRadius :Single;
-       procedure SetRadius( const Radius_:Single );
-     public
-       constructor Create( Owner_:TComponent ); override;
-       destructor Destroy; override;
-       ///// P R O P E R T Y
-       property Radius :Single read GetRadius write SetRadius;
-     end;
 
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin3D
 
@@ -133,169 +73,9 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        property Color  :TAlphaColorF read GetColor  write SetColor ;
      end;
 
-//const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C O N S T A N T 】
-
-var //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 V A R I A B L E 】
-
-    _World3D  :TWorld3D;
-    _Camera3D :TCamera3D;
-    _Light3D  :TLight3D;
-    _Sphere3D :TSphere3D;
-    _Poins3D  :TPoins3D;
-    _Plots3D0 :TPoins3D;
-    _Plots3D1 :TPoins3D;
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
-
-procedure MakeScene;
-
 implementation //############################################################### ■
 
-uses Main;
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
-
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TWorld3D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCamera3D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////// A C C E S S O R
-
-function TCamera3D.GetRadius :Single;
-begin
-     Result := _Radius;
-end;
-
-procedure TCamera3D.SetRadius( const Radius_:Single );
-begin
-     _Radius := Radius_;
-
-     MakePose;
-end;
-
-//------------------------------------------------------------------------------
-
-function TCamera3D.GetAngleX :Single;
-begin
-     Result := _AngleX;
-end;
-
-procedure TCamera3D.SetAngleX( const AngleX_:Single );
-begin
-     _AngleX := AngleX_;
-
-     MakePose;
-end;
-
-function TCamera3D.GetAngleY :Single;
-begin
-     Result := _AngleY;
-end;
-
-procedure TCamera3D.SetAngleY( const AngleY_:Single );
-begin
-     _AngleY := AngleY_;
-
-     MakePose;
-end;
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-procedure TCamera3D.MakePose;
-begin
-     Pose := TSingleM4.RotateY( +_AngleX )
-           * TSingleM4.RotateX( -_AngleY )
-           * TSingleM4.Translate( 0, 0, _Radius );
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TCamera3D.Create( Owner_:TComponent );
-begin
-     inherited;
-
-     Camera.AngleOfView := 8;
-
-     Radius := 100;
-     AngleX := 0;
-     AngleY := DegToRad( 45 );
-end;
-
-destructor TCamera3D.Destroy;
-begin
-
-     inherited;
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TLight3D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TLight3D.Create( Owner_:TComponent );
-begin
-     inherited;
-
-     Pose := TSingleM4.RotateX( DegToRad( 135 ) );
-end;
-
-destructor TLight3D.Destroy;
-begin
-
-     inherited;
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSphere3D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////// A C C E S S O R
-
-function TSphere3D.GetRadius :Single;
-begin
-     Result := SizeX / 2;
-end;
-
-procedure TSphere3D.SetRadius( const Radius_:Single );
-begin
-     SizeX := Radius_ * 2;
-     SizeY := SizeX;
-     SizeZ := SizeX;
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TSphere3D.Create( Owner_:TComponent );
-begin
-     inherited;
-
-     Radius := 4.8;
-     DivX   := 180;
-     DivY   := 90;
-end;
-
-destructor TSphere3D.Destroy;
-begin
-
-     inherited;
-end;
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin3D
 
@@ -484,39 +264,6 @@ begin
      Poins := nil;
 
      inherited;
-end;
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
-
-procedure MakeScene;
-begin
-     with Form1 do
-     begin
-          _World3D := TWorld3D.Create( Viewport3D1 );
-
-          _Camera3D := TCamera3D.Create( _World3D );
-
-          Viewport3D1.Camera := _Camera3D.Camera;
-
-          _Light3D := TLight3D.Create( _Camera3D );
-
-          _Sphere3D := TSphere3D.Create( _World3D );
-          _Sphere3D.Material.Texture.LoadFromFile( '..\..\_DATA\Sphere 1800x900.png' );
-
-          _Poins3D := TPoins3D.Create( _World3D );
-          _Poins3D.Poins := _Poins3S;
-          _Poins3D.Color := TAlphaColorF.Create( 1, 0, 0 );
-
-          _Plots3D0 := TPoins3D.Create( _World3D );
-          _Plots3D0.Poins := _Plots3S0;
-          _Plots3D0.Size := 0.2;
-          _Plots3D0.Color := TAlphaColorF.Create( 0, 1, 0 );
-
-          _Plots3D1 := TPoins3D.Create( _World3D );
-          _Plots3D1.Poins := _Plots3S1;
-          _Plots3D1.Size := 0.2;
-          _Plots3D1.Color := TAlphaColorF.Create( 0, 0, 1 );
-     end;
 end;
 
 end. //######################################################################### ■
