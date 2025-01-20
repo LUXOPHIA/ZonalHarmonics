e032c6b2d1b0c4624310e28428fc867b1d5c9274
luxidea <contact@luxidea.net>
2025-01-20 15:55:08 +0900

    TPoins3D クラスを廃止。 TPoins3D クラスを、S3 版から移植。

--- S2/FMX/LUX.Sphere.FMX.S2.pas
+++ S2/FMX/LUX.Sphere.FMX.S2.pas
@@ -3,185 +3,89 @@
 interface //#################################################################### ■
 
 uses System.Types, System.SysUtils, System.RTLConsts, System.Classes,
-     System.UITypes, System.Math, System.Math.Vectors,
+     System.UITypes, System.Math.Vectors,
      FMX.Types3D, FMX.Controls3D, FMX.MaterialSources,
-     LUX, LUX.D3,
+     LUX, LUX.D3, LUX.Quaternion, LUX.D4x4,
      LUX.FMX.Graphics.D3,
-     LUX.Sphere.FMX,
      LIB.Poins.S2;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin3D
-
-     TPoin3D = class( TF3DObject )
-     private
-     protected
-       _Sphere :TSphere3D;
-       _Direct :TSingle3D;
-       _Radius :Single;
-       ///// A C C E S S O R
-       function GetRadius :Single;
-       procedure SetRadius( const Radius_:Single );
-       function GetSize :Single;
-       procedure SetSize( const Size_:Single );
-       function GetColor :TAlphaColorF;
-       procedure SetColor( const Color_:TAlphaColorF );
-       function GetDirect :TSingle3D;
-       procedure SetDirect( const Direct_:TSingle3D );
-       ///// M E T H O D
-       procedure MakeModel; override;
-     public
-       constructor Create( Owner_:TComponent ); override;
-       destructor Destroy; override;
-       ///// P R O P E R T Y
-       property Radius :Single       read GetRadius write SetRadius;
-       property Size   :Single       read GetSize   write SetSize  ;
-       property Color  :TAlphaColorF read GetColor  write SetColor ;
-       property Direct :TSingle3D    read GetDirect write SetDirect;
-     end;
-
      //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoins3D
 
-     TPoins3D = class( TF3DObject )
+     TPoins3D = class( TF3DShaper )
      private
+       ///// M E T H O D
+       function XYtoI( const I_,X_,Y_:Integer ) :Integer; inline;
      protected
-       _Poins  :TPoins2S;
-       _Radius :Single;
-       _Size   :Single;
-       _Color  :TAlphaColorF;
+       _Polygons :TMeshData;
+       _Material :TLightMaterialSource;
+       _Poins    :TPoins2S;
+       _Radius   :Single;
+       _PlotR    :Single;
+       _DivX     :Integer;
+       _DivY     :Integer;
        ///// A C C E S S O R
+       function GetMaterial :TLightMaterialSource;
        procedure UpPoins( Sender_:TObject );
        function GetPoins :TPoins2S;
        procedure SetPoins( const Poins_:TPoins2S );
        function GetRadius :Single;
        procedure SetRadius( const Radius_:Single );
-       function GetColor :TAlphaColorF;
-       procedure SetColor( const Color_:TAlphaColorF );
+       function GetPlotR :Single;
+       procedure SetPlotR( const PlotR_:Single );
+       function GetDivX :Integer;
+       procedure SetDivX( const DivX_:Integer );
+       function GetDivY :Integer;
+       procedure SetDivY( const DivY_:Integer );
        ///// M E T H O D
-       procedure MakeModel; override;
+       procedure Render; override;
+       procedure MakeGeometry; override;
+       procedure MakeTopology; override;
      public
        constructor Create( Owner_:TComponent ); override;
        destructor Destroy; override;
        ///// P R O P E R T Y
-       property Poins  :TPoins2S     read GetPoins  write SetPoins ;
-       property Radius :Single       read GetRadius write SetRadius;
-       property Size   :Single       read   _Size   write   _Size  ;
-       property Color  :TAlphaColorF read GetColor  write SetColor ;
+       property Material :TLightMaterialSource read GetMaterial                ;
+       property Poins    :TPoins2S             read GetPoins    write SetPoins ;
+       property Radius   :Single               read GetRadius   write SetRadius;
+       property PlotR    :Single               read GetPlotR    write SetPlotR ;
+       property DivX     :Integer              read GetDivX     write SetDivX  ;
+       property DivY     :Integer              read GetDivY     write SetDivY  ;
      end;
 
 implementation //############################################################### ■
 
+uses System.Math;
+
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoin3D
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////// A C C E S S O R
-
-function TPoin3D.GetRadius :Single;
-begin
-     Result := _Radius;
-end;
-
-procedure TPoin3D.SetRadius( const Radius_:Single );
-begin
-     _Radius := Radius_;  upModel := True;
-end;
-
-//------------------------------------------------------------------------------
-
-function TPoin3D.GetSize :Single;
-begin
-     Result := _Sphere.SizeX;
-end;
-
-procedure TPoin3D.SetSize( const Size_:Single );
-begin
-     _Sphere.SizeX := Size_;
-     _Sphere.SizeY := Size_;
-     _Sphere.SizeZ := Size_;
-end;
-
-//------------------------------------------------------------------------------
-
-function TPoin3D.GetColor :TAlphaColorF;
-begin
-     Result := TAlphaColorF.Create( _Sphere.Material.Diffuse );
-end;
-
-procedure TPoin3D.SetColor( const Color_:TAlphaColorF );
-var
-   D, A :TAlphaColorF;
-begin
-     D := 0.8 * Color_;
-     A := 0.2 * Color_;
-
-     _Sphere.Material.Diffuse := D.ToAlphaColor;
-     _Sphere.Material.Ambient := A.ToAlphaColor;
-end;
-
-//------------------------------------------------------------------------------
-
-function TPoin3D.GetDirect :TSingle3D;
-begin
-     Result := _Direct;
-end;
-
-procedure TPoin3D.SetDirect( const Direct_:TSingle3D );
-begin
-     _Direct := Direct_;  upModel := True;
-end;
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-procedure TPoin3D.MakeModel;
-begin
-     _Sphere.Pos := _Direct * _Radius;
-end;
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
-
-constructor TPoin3D.Create( Owner_:TComponent );
-begin
-     inherited;
-
-     _Sphere := TSphere3D.Create( Self );
-
-     with _Sphere do
-     begin
-          Parent := Self;
-          DivX   := 18;
-          DivY   := 9;
-     end;
-
-     Size   := 0.4;
-     Direct := TSingle3D.Create( 1, 0, 0 );
-     Radius := 5;
-end;
-
-destructor TPoin3D.Destroy;
-begin
-
-     inherited;
-end;
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TPoins3D
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& private
 
+function TPoins3D.XYtoI( const I_,X_,Y_:Integer ) :Integer;
+begin
+     Result := ( ( _DivY + 1 ) * I_ + Y_ ) * ( _DivX + 1 ) + X_;
+end;
+
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
 
 //////////////////////////////////////////////////////////////// A C C E S S O R
 
+function TPoins3D.GetMaterial :TLightMaterialSource;
+begin
+     Result := _Material;
+end;
+
+//------------------------------------------------------------------------------
+
 procedure TPoins3D.UpPoins( Sender_:TObject );
 begin
-     upModel := True;
+     upGeometry := True;
+     upTopology := True;
 
      Repaint;
 end;
@@ -211,38 +115,145 @@ end;
 
 procedure TPoins3D.SetRadius( const Radius_:Single );
 begin
-     _Radius := Radius_;  upModel := True;
+     _Radius := Radius_;
+
+     upGeometry := True;
+     upTopology := True;
 end;
 
 //------------------------------------------------------------------------------
 
-function TPoins3D.GetColor :TAlphaColorF;
+function TPoins3D.GetPlotR :Single;
 begin
-     Result := _Color;
+     Result := _PlotR;
 end;
 
-procedure TPoins3D.SetColor( const Color_:TAlphaColorF );
+procedure TPoins3D.SetPlotR( const PlotR_:Single );
 begin
-     _Color := Color_;  upModel := True;
+     _PlotR := PlotR_;
+
+     upGeometry := True;
+     upTopology := True;
+end;
+
+//------------------------------------------------------------------------------
+
+function TPoins3D.GetDivX :Integer;
+begin
+     Result := _DivX;
+end;
+
+procedure TPoins3D.SetDivX( const DivX_:Integer );
+begin
+     _DivX := DivX_;
+
+     upGeometry := True;
+     upTopology := True;
+end;
+
+function TPoins3D.GetDivY :Integer;
+begin
+     Result := _DivY;
+end;
+
+procedure TPoins3D.SetDivY( const DivY_:Integer );
+begin
+     _DivY := DivY_;
+
+     upGeometry := True;
+     upTopology := True;
 end;
 
 //////////////////////////////////////////////////////////////////// M E T H O D
 
-procedure TPoins3D.MakeModel;
-var
-   I :Integer;
+procedure TPoins3D.Render;
 begin
-     Self.DeleteChildren;
+     inherited;
 
-     if not Assigned( _Poins ) then Exit;
+     _Polygons.Render( Context, TMaterialSource.ValidMaterial( _Material ), AbsoluteOpacity );
+end;
 
-     for I := 0 to _Poins.PoinsN-1 do
+procedure TPoins3D.MakeGeometry;
+var
+   M, M0, M1 :TMatrix3D;
+   N, X, Y, I :Integer;
+   T :TPointF;
+   P :TPoint3D;
+   R :Single;
+begin
+     M := TMatrix3D( TSingleM4.Translate( 0, Radius, 0 )
+                   * TSingleM4.Scale( _PlotR, _PlotR, _PlotR ) );
+
+     with _Polygons.VertexBuffer do
      begin
-          with TPoin3D.Create( Self ) do
+          Length := (DivY+1)*(DivX+1){Vert/Poin} * Poins.PoinsN{Poin};
+
+          for N := 0 to Poins.PoinsN{Cube}-1 do
           begin
-               Size   := _Size;
-               Color  := _Color;
-               Direct := _Poins[ I ];
+               M0 := TMatrix3D( TSingleM4( TSingleQ.Rotate( TDouble3D.IdentityY, Poins[ N ] ) ) );
+               M1 := M * M0;
+
+               for Y := 0 to DivY do
+               begin
+                    T.Y := Pi / DivY * Y;
+
+                    P.Y := Cos( T.Y );
+                    R   := Sin( T.Y );
+
+                    for X := 0 to DivX do
+                    begin
+                         T.X := Pi2 / DivX * X;
+
+                         P.X := +R * Cos( T.X );
+                         P.Z := -R * Sin( T.X );
+
+                         I := XYtoI( N, X, Y );
+
+                         Vertices [ I ] := P * M1;
+                         Normals  [ I ] := P * M0;
+                         TexCoord0[ I ] := TPointF.Create( X / DivX, Y / DivY );
+                    end;
+               end;
+          end;
+     end;
+end;
+
+procedure TPoins3D.MakeTopology;
+var
+   I, N, X0, X1, Y0, Y1 :Integer;
+begin
+     with _Polygons.IndexBuffer do
+     begin
+          Length := 3{Vert/Tria} * 2{Tria/Face} * DivY*DivX{Face/Poin} * _Poins.PoinsN{Poin};
+
+          I := 0;
+          for N := 0 to _Poins.PoinsN-1 do
+          begin
+               Y0 := 0;
+               for Y1 := 1 to _DivY do
+               begin
+                    X0 := 0;
+                    for X1 := 1 to _DivX do
+                    begin
+                         //     X0     X1
+                         //  Y0 +------+
+                         //     |＼    |
+                         //     |  ＼  |
+                         //     |    ＼|
+                         //  Y1 +------+
+
+                         Indices[ I ] := XYtoI( N, X0, Y0 );  Inc( I );
+                         Indices[ I ] := XYtoI( N, X1, Y0 );  Inc( I );
+                         Indices[ I ] := XYtoI( N, X1, Y1 );  Inc( I );
+
+                         Indices[ I ] := XYtoI( N, X1, Y1 );  Inc( I );
+                         Indices[ I ] := XYtoI( N, X0, Y1 );  Inc( I );
+                         Indices[ I ] := XYtoI( N, X0, Y0 );  Inc( I );
+
+                         X0 := X1;
+                    end;
+                    Y0 := Y1;
+               end;
           end;
      end;
 end;
@@ -253,12 +264,22 @@ constructor TPoins3D.Create( Owner_:TComponent );
 begin
      inherited;
 
-     Size := 0.4;
+     _Polygons := TMeshData.Create;
+
+     _Material := TLightMaterialSource.Create( Self );
+     _Material.Specular := TAlphaColors.Null;
+
+     Radius := 5;
+     PlotR  := 0.3;
+     DivX   := 18;
+     DivY   := 9;
 end;
 
 destructor TPoins3D.Destroy;
 begin
      Poins := nil;
+
+     _Polygons.Free;
 
      inherited;
 end;
