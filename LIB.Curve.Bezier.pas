2ea5561da7daa932219ceeb88b7b46fb74093c36
luxidea <contact@luxidea.net>
2025-01-21 14:50:38 +0900

    TSingleBezier / TDoubleBezier クラスを追加。

--- LIB.Curve.Bezier.pas
+++ LIB.Curve.Bezier.pas
@@ -2,11 +2,29 @@
 
 interface //#################################################################### ■
 
-//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+uses LIB.Curve;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBezier<_TValue_>
+
+     TSingleBezier<_TValue_> = class
+     public
+       ///// M E T H O D
+       class function CurveREC( Ps:TArray<_TValue_>; const t:Single; const DegN_:Integer; const Lerp_:TSingleLerp<_TValue_> ) :_TValue_;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDoubleBezier<_TValue_>
+
+     TDoubleBezier<_TValue_> = class
+     public
+       ///// M E T H O D
+       class function CurveREC( Ps:TArray<_TValue_>; const t:Double; const DegN_:Integer; const Lerp_:TDoubleLerp<_TValue_> ) :_TValue_;
+     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
@@ -20,6 +38,42 @@ uses System.Math,
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TSingleBezier<_TValue_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+class function TSingleBezier<_TValue_>.CurveREC( Ps:TArray<_TValue_>; const t:Single; const DegN_:Integer; const Lerp_:TSingleLerp<_TValue_> ) :_TValue_;
+var
+   N, I :Integer;
+begin
+     for N := DegN_-1 downto 0 do
+     begin
+          for I := 0 to N do Ps[ I ] := Lerp_( Ps[ I ], Ps[ I+1 ], t );
+     end;
+
+     Result := Ps[ 0 ];
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TDoubleBezier<_TValue_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+class function TDoubleBezier<_TValue_>.CurveREC( Ps:TArray<_TValue_>; const t:Double; const DegN_:Integer; const Lerp_:TDoubleLerp<_TValue_> ) :_TValue_;
+var
+   N, I :Integer;
+begin
+     for N := DegN_-1 downto 0 do
+     begin
+          for I := 0 to N do Ps[ I ] := Lerp_( Ps[ I ], Ps[ I+1 ], t );
+     end;
+
+     Result := Ps[ 0 ];
+end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
