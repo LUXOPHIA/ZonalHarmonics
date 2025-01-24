ce546113b1a987f93ced9f0b530b30be95dc11c0
luxidea <contact@luxidea.net>
2025-01-24 22:18:16 +0900

    TCurveCatmullRomREC, TCurveCatmullRomAVE クラスを追加。

--- LIB.Curve.CatmullRom.pas
+++ LIB.Curve.CatmullRom.pas
@@ -2,8 +2,7 @@
 
 interface //#################################################################### ■
 
-uses LUX.D4,
-     LIB.Curve;
+uses LIB.Curve;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
@@ -11,18 +10,38 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRom<_TValue_>
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRom<_TPoin_>
 
      TCurveCatmullRom<_TPoin_> = class( TCurve<_TPoin_> )
+     private
+     protected
      public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomREC<_TPoin_>
+
+     TCurveCatmullRomREC<_TPoin_> = class( TCurveCatmullRom<_TPoin_> )
+     private
+     protected
        ///// M E T H O D
-       function CurveREC( Ps:TArray<_TPoin_>; const t:Double; const Lerp_:TDoubleLerp<_TPoin_> ) :_TPoin_;
+       function Segment( const i:Integer; const t:Double ) :_TPoin_; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomAVE<_TPoin_>
+
+     TCurveCatmullRomAVE<_TPoin_> = class( TCurveCatmullRom<_TPoin_> )
+     private
+     protected
+       ///// M E T H O D
+       function Segment( const i:Integer; const t:Double ) :_TPoin_; override;
+     public
      end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
-function CatmullRom( const t:Single ) :TSingle4D; overload;
-function CatmullRom( const t:Double ) :TDouble4D; overload;
+function CatmullRom( const t:Single ) :TArray<Single>; overload;
+function CatmullRom( const t:Double ) :TArray<Double>; overload;
 
 implementation //############################################################### ■
 
@@ -30,42 +49,80 @@ implementation //###############################################################
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRom<_TValue_>
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRom<_TPoin_>
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomREC<_TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
 
 //////////////////////////////////////////////////////////////////// M E T H O D
 
-function TCurveCatmullRom<_TPoin_>.CurveREC( Ps:TArray<_TPoin_>; const t:Double; const Lerp_:TDoubleLerp<_TPoin_> ) :_TPoin_;
+function TCurveCatmullRomREC<_TPoin_>.Segment( const i:Integer; const t:Double ) :_TPoin_;
+var
+   Ps :TArray<_TPoin_>;
 begin
-     Ps[0] := Lerp_( Ps[0], Ps[1],   t + 1       );
-     Ps[1] := Lerp_( Ps[1], Ps[2],   t           );
-     Ps[2] := Lerp_( Ps[2], Ps[3],   t - 1       );
+     SetLength( Ps, 4 );
 
-     Ps[0] := Lerp_( Ps[0], Ps[1], ( t + 1 ) / 2 );
-     Ps[1] := Lerp_( Ps[1], Ps[2],   t       / 2 );
+     Ps[0] := Poins[ i-1 ];
+     Ps[1] := Poins[ i   ];
+     Ps[2] := Poins[ i+1 ];
+     Ps[3] := Poins[ i+2 ];
 
-     Ps[0] := Lerp_( Ps[0], Ps[1],   t           );
+     Ps[0] := Bary.Lerp( Ps[0], Ps[1],   t + 1       );
+     Ps[1] := Bary.Lerp( Ps[1], Ps[2],   t           );
+     Ps[2] := Bary.Lerp( Ps[2], Ps[3],   t - 1       );
+
+     Ps[0] := Bary.Lerp( Ps[0], Ps[1], ( t + 1 ) / 2 );
+     Ps[1] := Bary.Lerp( Ps[1], Ps[2],   t       / 2 );
+
+     Ps[0] := Bary.Lerp( Ps[0], Ps[1],   t           );
 
      Result := Ps[0];
 end;
 
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomAVE<_TPoin_>
 
-function CatmullRom( const t:Single ) :TSingle4D;
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+function TCurveCatmullRomAVE<_TPoin_>.Segment( const i:Integer; const t:Double ) :_TPoin_;
+var
+   Ps :TArray<TWector>;
+   Ws :TArray<Double>;
 begin
-     Result[1] := ( ( -0.5 * t + 1.0 ) * t - 0.5 ) * t      ;
-     Result[2] := ( ( +1.5 * t - 2.5 ) * t       ) * t + 1.0;
-     Result[3] := ( ( -1.5 * t + 2.0 ) * t + 0.5 ) * t      ;
-     Result[4] := ( ( +0.5 * t - 0.5 ) * t       ) * t      ;
+     SetLength( Ps, 4 );
+
+     Ws := CatmullRom( t );
+
+     Ps[0] := TWector.Create( Poins[ i-1 ], Ws[0] );
+     Ps[1] := TWector.Create( Poins[ i   ], Ws[1] );
+     Ps[2] := TWector.Create( Poins[ i+1 ], Ws[2] );
+     Ps[3] := TWector.Create( Poins[ i+2 ], Ws[3] );
+
+     Result := Bary.Lerp( Ps );
 end;
 
-function CatmullRom( const t:Double ) :TDouble4D;
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+function CatmullRom( const t:Single ) :TArray<Single>;
 begin
-     Result[1] := ( ( -0.5 * t + 1.0 ) * t - 0.5 ) * t      ;
-     Result[2] := ( ( +1.5 * t - 2.5 ) * t       ) * t + 1.0;
-     Result[3] := ( ( -1.5 * t + 2.0 ) * t + 0.5 ) * t      ;
-     Result[4] := ( ( +0.5 * t - 0.5 ) * t       ) * t      ;
+     SetLength( Result, 4 );
+
+     Result[0] := ( ( -0.5 * t + 1.0 ) * t - 0.5 ) * t      ;
+     Result[1] := ( ( +1.5 * t - 2.5 ) * t       ) * t + 1.0;
+     Result[2] := ( ( -1.5 * t + 2.0 ) * t + 0.5 ) * t      ;
+     Result[3] := ( ( +0.5 * t - 0.5 ) * t       ) * t      ;
+end;
+
+function CatmullRom( const t:Double ) :TArray<Double>;
+begin
+     SetLength( Result, 4 );
+
+     Result[0] := ( ( -0.5 * t + 1.0 ) * t - 0.5 ) * t      ;
+     Result[1] := ( ( +1.5 * t - 2.5 ) * t       ) * t + 1.0;
+     Result[2] := ( ( -1.5 * t + 2.0 ) * t + 0.5 ) * t      ;
+     Result[3] := ( ( +0.5 * t - 0.5 ) * t       ) * t      ;
 end;
 
 end. //######################################################################### ■
