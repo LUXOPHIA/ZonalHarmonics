a7922ddedbbc4f30611b35c5e3cc3b5f5468f294
luxidea <contact@luxidea.net>
2025-01-24 22:12:33 +0900

    TCurveBezierREC3S, TCurveBezierAVE3S クラスに改名。

--- S3/LUX.Curve.S3.Bezier.pas
+++ S3/LUX.Curve.S3.Bezier.pas
@@ -3,37 +3,18 @@
 interface //#################################################################### ■
 
 uses LIB.Curve.Bezier,
-     LUX.S3,
-     LUX.Poins.S3,
-     LUX.Curve.S3;
+     LUX.S3;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      TCurveBezier3S = TCurveBezier<TDouble3S>;
 
+     TCurveBezierREC3S = TCurveBezierREC<TDouble3S>;
+     TCurveBezierAVE3S = TCurveBezierAVE<TDouble3S>;
+
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBezierREC
-
-     TCurveBezierREC = class( TCurveBezier3S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble3S; override;
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBezierAVE
-
-     TCurveBezierAVE = class( TCurveBezier3S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble3S; override;
-     public
-     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
@@ -42,42 +23,6 @@ implementation //###############################################################
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBezierREC
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveBezierREC.Segment( const i:Integer; const t:Double ) :TDouble3S;
-var
-   Ps :TArray<TDouble3S>;
-   J :Integer;
-begin
-     SetLength( Ps, DegN+1 );
-
-     for J := 0 to DegN do Ps[ J ] := _Poins[ i + J ];
-
-     Result := CurveREC( Ps, t, Slerp );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBezierAVE
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveBezierAVE.Segment( const i:Integer; const t:Double ) :TDouble3S;
-var
-   Ps :TArray<TDouble3Sw>;
-   J :Integer;
-begin
-     SetLength( Ps, DegN+1 );
-
-     for J := 0 to DegN do Ps[ J ] := TDouble3Sw.Create( _Poins[ i+J ], Bezier( DegN, J, t ) );
-
-     Result := TDouble3S( Sum1D( Ps ) );
-end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
