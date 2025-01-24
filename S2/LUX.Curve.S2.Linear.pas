adc8132bf92adcea3cf188ebc00ef8a4d7afb07d
luxidea <contact@luxidea.net>
2025-01-24 22:06:28 +0900

    TCurveLinear → TCurveLinear2S クラスに改名。 TCurveLinearREC → TCurveLinearREC2S TCurveLinearAVE → TCurveLinearAVE2S

--- S2/LUX.Curve.S2.Linear.pas
+++ S2/LUX.Curve.S2.Linear.pas
@@ -2,42 +2,19 @@
 
 interface //#################################################################### ■
 
-uses LUX.S2,
-     LUX.Curve.S2;
+uses LIB.Curve.Linear,
+     LUX.S2;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     TCurveLinear2S = TCurveLinear<TDouble2S>;
+
+     TCurveLinearREC2S = TCurveLinearREC<TDouble2S>;
+     TCurveLinearAVE2S = TCurveLinearAVE<TDouble2S>;
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinear
-
-     TCurveLinear = class( TCurve2S )
-     private
-     protected
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearREC
-
-     TCurveLinearREC = class( TCurveLinear )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble2S; override;
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearAVE
-
-     TCurveLinearAVE = class( TCurveLinear )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble2S; override;
-     public
-     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
@@ -46,42 +23,6 @@ implementation //###############################################################
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinear
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearREC
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveLinearREC.Segment( const i:Integer; const t:Double ) :TDouble2S;
-var
-   P0, P1 :TDouble2S;
-begin
-     P0 := _Poins[ i   ];
-     P1 := _Poins[ i+1 ];
-
-     Result := Slerp( P0, P1, t );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearAVE
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveLinearAVE.Segment( const i:Integer; const t:Double ) :TDouble2S;
-var
-   Ps :TArray<TDouble2Sw>;
-begin
-     SetLength( Ps, 2 );
-
-     Ps[0] := TDouble2Sw.Create( _Poins[ i   ], 1 - t );
-     Ps[1] := TDouble2Sw.Create( _Poins[ i+1 ],     t );
-
-     Result := TDouble2S( Sum1D( Ps ) );
-end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
