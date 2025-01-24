a8d4c46e55556ed392454c55e1ef3727fb631481
luxidea <contact@luxidea.net>
2025-01-24 22:02:57 +0900

    LIB.Curve.Linear ユニットを追加。 TCurveLinear, TCurveLinearREC, TCurveLinearAVE クラスを追加。

---
+++ LIB.Curve.Linear.pas
@@ -0,0 +1,85 @@
+﻿unit LIB.Curve.Linear;
+
+interface //#################################################################### ■
+
+uses LIB.Curve;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinear<_TPoin_>
+
+     TCurveLinear<_TPoin_> = class( TCurve<_TPoin_> )
+     private
+     protected
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearREC<_TPoin_>
+
+     TCurveLinearREC<_TPoin_> = class( TCurveLinear<_TPoin_> )
+     private
+     protected
+       ///// M E T H O D
+       function Segment( const i:Integer; const t:Double ) :_TPoin_; override;
+     public
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearAVE<_TPoin_>
+
+     TCurveLinearAVE<_TPoin_> = class( TCurveLinear<_TPoin_> )
+     private
+     protected
+       ///// M E T H O D
+       function Segment( const i:Integer; const t:Double ) :_TPoin_; override;
+     public
+     end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+implementation //############################################################### ■
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinear
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearREC<_TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+function TCurveLinearREC<_TPoin_>.Segment( const i:Integer; const t:Double ) :_TPoin_;
+var
+   P0, P1 :_TPoin_;
+begin
+     P0 := _Poins[ i   ];
+     P1 := _Poins[ i+1 ];
+
+     Result := Bary.Lerp( P0, P1, t );
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLinearAVE<_TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+function TCurveLinearAVE<_TPoin_>.Segment( const i:Integer; const t:Double ) :_TPoin_;
+var
+   Ps :TArray<TWector>;
+begin
+     Ps := [ TWector.Create( _Poins[ i   ], 1 - t ),
+             TWector.Create( _Poins[ i+1 ],     t ) ];
+
+     Result := Bary.Lerp( Ps );
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+end. //######################################################################### ■
