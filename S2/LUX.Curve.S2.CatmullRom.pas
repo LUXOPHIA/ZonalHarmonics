eb291b5980f2c0085e8accc59f443a5e27d62ff9
luxidea <contact@luxidea.net>
2025-01-24 22:18:56 +0900

    TCurveCatmullRomREC2S, TCurveCatmullRomAVE2S クラスに改名。

--- S2/LUX.Curve.S2.CatmullRom.pas
+++ S2/LUX.Curve.S2.CatmullRom.pas
@@ -3,89 +3,26 @@
 interface //#################################################################### ■
 
 uses LIB.Curve.CatmullRom,
-     LUX.S2,
-     LUX.Curve.S2;
+     LUX.S2;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      TCurveCatmullRom2S = TCurveCatmullRom<TDouble2S>;
 
+     TCurveCatmullRomREC2S = TCurveCatmullRomREC<TDouble2S>;
+     TCurveCatmullRomAVE2S = TCurveCatmullRomAVE<TDouble2S>;
+
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomREC
-
-     TCurveCatmullRomREC = class( TCurveCatmullRom2S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble2S; override;
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomAVE
-
-     TCurveCatmullRomAVE = class( TCurveCatmullRom2S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble2S; override;
-     public
-     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
 implementation //############################################################### ■
 
-uses LUX.D4;
-
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomREC
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveCatmullRomREC.Segment( const i:Integer; const t:Double ) :TDouble2S;
-var
-   Ps :TArray<TDouble2S>;
-begin
-     SetLength( Ps, 4 );
-
-     Ps[0] := _Poins[ i-1 ];
-     Ps[1] := _Poins[ i   ];
-     Ps[2] := _Poins[ i+1 ];
-     Ps[3] := _Poins[ i+2 ];
-
-     Result := CurveREC( Ps, t, Slerp );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveCatmullRomAVE
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveCatmullRomAVE.Segment( const i:Integer; const t:Double ) :TDouble2S;
-var
-   Ws :TDouble4D;
-   Ps :TArray<TDouble2Sw>;
-begin
-     Ws := CatmullRom( t );
-
-     SetLength( Ps, 4 );
-
-     Ps[0] := TDouble2Sw.Create( _Poins[ i-1 ], Ws[1] );
-     Ps[1] := TDouble2Sw.Create( _Poins[ i   ], Ws[2] );
-     Ps[2] := TDouble2Sw.Create( _Poins[ i+1 ], Ws[3] );
-     Ps[3] := TDouble2Sw.Create( _Poins[ i+2 ], Ws[4] );
-
-     Result := TDouble2S( Sum1D( Ps ) );
-end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
