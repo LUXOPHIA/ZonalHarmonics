b3f89005fd24b77e46e41d9bdb2667525bdab214
luxidea <contact@luxidea.net>
2025-01-24 22:16:52 +0900

    TCurveBSplineREC3S, TCurveBSplineAVE3S クラスに改名。

--- S3/LUX.Curve.S3.BSpline.pas
+++ S3/LUX.Curve.S3.BSpline.pas
@@ -3,37 +3,18 @@
 interface //#################################################################### ■
 
 uses LIB.Curve.BSpline,
-     LUX.S3,
-     LUX.Poins.S3,
-     LUX.Curve.S3;
+     LUX.S3;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
      TCurveBSpline3S = TCurveBSpline<TDouble3S>;
 
+     TCurveBSplineREC3S = TCurveBSplineREC<TDouble3S>;
+     TCurveBSplineAVE3S = TCurveBSplineAVE<TDouble3S>;
+
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBSplineREC
-
-     TCurveBSplineREC = class( TCurveBSpline3S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble3S; override;
-     public
-     end;
-
-     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBSplineAVE
-
-     TCurveBSplineAVE = class( TCurveBSpline3S )
-     private
-     protected
-       ///// M E T H O D
-       function Segment( const i:Integer; const t:Double ) :TDouble3S; override;
-     public
-     end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
@@ -42,42 +23,6 @@ implementation //###############################################################
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBSplineREC
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveBSplineREC.Segment( const i:Integer; const t:Double ) :TDouble3S;
-var
-   Ps :TArray<TDouble3S>;
-   J :Integer;
-begin
-     SetLength( Ps, DegN+1 );
-
-     for J := 0 to DegN do Ps[ J ] := _Poins[ i + J ];
-
-     Result := CurveREC( Ps, t, Slerp );
-end;
-
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveBSplineAVE
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
-
-//////////////////////////////////////////////////////////////////// M E T H O D
-
-function TCurveBSplineAVE.Segment( const i:Integer; const t:Double ) :TDouble3S;
-var
-   Ps :TArray<TDouble3Sw>;
-   J :Integer;
-begin
-     SetLength( Ps, DegN+1 );
-
-     for J := 0 to DegN do Ps[ J ] := TDouble3Sw.Create( _Poins[ i+J ], BSpline( DegN, J, t ) );
-
-     Result := TDouble3S( Sum1D( Ps ) );
-end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
 
