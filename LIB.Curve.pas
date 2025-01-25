e7c38400758cae8726771e297bd4188915eb834d
luxidea <contact@luxidea.net>
2025-01-26 01:19:26 +0900

    TAlgoFunc 型を追加。 TCurveAlgo クラスを追加。

--- LIB.Curve.pas
+++ LIB.Curve.pas
@@ -8,8 +8,9 @@ uses LUX,
 
 type //$$$$$$$$$&&$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
-     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+     TAlgoFunc<_TPoin_> = function ( const i:Integer; const t:Double ) :_TPoin_ of object;
 
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
 
      //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
 
@@ -35,7 +36,7 @@ type //$$$$$$$$$&&$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        ///// M E T H O D
        function Segment( const I:Integer; const T:Double ) :_TPoin_; virtual; abstract;
      public
-       constructor Create( const Poins_:TPoins );
+       constructor Create;
        destructor Destroy; Override;
        ///// P R O P E R T Y
        property Bary  :TBarycenter read GetBary  write SetBary ;
@@ -44,6 +45,29 @@ type //$$$$$$$$$&&$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        function Value( const X_:Double ) :_TPoin_; virtual;
        ///// E V E N T
        property OnChange :TDelegates read _OnChange;
+     end;
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveAlgo<_TPoin_>
+
+     TCurveAlgo<_TPoin_> = class( TCurve<_TPoin_> )
+     private
+     protected
+       type TAlgoFunc = TAlgoFunc<_TPoin_>;
+     protected
+       _Algos  :TArray<TAlgoFunc>;
+       _AlgoID :Integer;
+       ///// A C C E S S O R
+       function GetAlgosN :Integer; virtual;
+       procedure SetAlgosN( const AlgosN_:Integer ); virtual;
+       function GetAlgoID :Integer; virtual;
+       procedure SetAlgoID( const AlgoID_:Integer ); virtual;
+       ///// M E T H O D
+       function Segment( const i:Integer; const t:Double ) :_TPoin_; override;
+     public
+       constructor Create;
+       ///// P R O P E R T Y
+       property AlgosN :Integer read GetAlgosN write SetAlgosN;
+       property AlgoID :Integer read GetAlgoID write SetAlgoID;
      end;
 
 //const //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C O N S T A N T 】
@@ -75,6 +99,8 @@ end;
 
 procedure TCurve<_TPoin_>.SetBary( const Bary_:TBarycenter );
 begin
+     if _Bary = Bary_ then Exit;
+
      _Bary := Bary_;  _OnChange.Run( Self );
 end;
 
@@ -92,6 +118,8 @@ end;
 
 procedure TCurve<_TPoin_>.SetPoins( const Poins_:TPoins );
 begin
+     if _Poins = Poins_ then Exit;
+
      if Assigned( _Poins ) then _Poins.OnChange.Del( UpPoins );
 
      _Poins := Poins_;
@@ -103,11 +131,12 @@ end;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
-constructor TCurve<_TPoin_>.Create( const Poins_:TPoins );
+constructor TCurve<_TPoin_>.Create;
 begin
-     inherited Create;
+     inherited;
 
-     Poins := Poins_;
+     Bary  := nil;
+     Poins := nil;
 end;
 
 destructor TCurve<_TPoin_>.Destroy;
@@ -127,6 +156,52 @@ begin
      i := Floor( X_ );  t := X_ - I;
 
      Result := Segment( i, t );
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveAlgo<_TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//////////////////////////////////////////////////////////////// A C C E S S O R
+
+function TCurveAlgo<_TPoin_>.GetAlgosN :Integer;
+begin
+     Result := Length( _Algos );
+end;
+
+procedure TCurveAlgo<_TPoin_>.SetAlgosN( const AlgosN_:Integer );
+begin
+     if AlgosN = AlgosN_ then Exit;
+
+     SetLength( _Algos, AlgosN_ );  _OnChange.Run( Self );
+end;
+
+function TCurveAlgo<_TPoin_>.GetAlgoID :Integer;
+begin
+     Result := _AlgoID;
+end;
+
+procedure TCurveAlgo<_TPoin_>.SetAlgoID( const AlgoID_:Integer );
+begin
+     if _AlgoID = AlgoID_ then Exit;
+
+     _AlgoID := AlgoID_;  _OnChange.Run( Self );
+end;
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+function TCurveAlgo<_TPoin_>.Segment( const i:Integer; const t:Double ) :_TPoin_;
+begin
+     Result := _Algos[ AlgoID ]( i, t );
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TCurveAlgo<_TPoin_>.Create;
+begin
+     inherited;
+
+     AlgoID := 0;
 end;
 
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
