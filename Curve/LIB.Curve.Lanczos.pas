79463d42b67ee92e04b7fba3937f3f1fe78e0eac
luxidea <contact@luxidea.net>
2025-01-28 12:43:27 +0900

    LIB.Curve.Lanczos, LUX.Curve.S2.Lanczos, LUX.Curve.S3.Lanczos ユニットを追加。

---
+++ Curve/LIB.Curve.Lanczos.pas
@@ -0,0 +1,119 @@
+﻿unit LIB.Curve.Lanczos;
+
+interface //#################################################################### ■
+
+uses LIB.Poins,
+     LIB.Curve;
+
+type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLanczos<_TPoin_>
+
+     TCurveLanczos<_TPoin_> = class( TCurveAlgo<_TPoin_> )
+     private
+     protected
+       _DegN :Integer;
+       ///// A C C E S S O R
+       function GetDegN :Integer; virtual;
+       procedure SetDegN( const DegN_:Integer ); virtual;
+       ///// M E T H O D
+       function Lanczos( const X_:Double ) :Double;
+       function Algorithm0( const i:Integer; const t:Double ) :_TPoin_;
+     public
+       constructor Create;
+       ///// P R O P E R T Y
+       property DegN :Integer read GetDegN write SetDegN;
+     end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+function Sinc( const X_:Single ) :Single; overload;
+function Sinc( const X_:Double ) :Double; overload;
+
+implementation //############################################################### ■
+
+uses LUX;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TCurveLanczos<_TPoin_>
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
+
+//////////////////////////////////////////////////////////////// A C C E S S O R
+
+function TCurveLanczos<_TPoin_>.GetDegN :Integer;
+begin
+     Result := _DegN;
+end;
+
+procedure TCurveLanczos<_TPoin_>.SetDegN( const DegN_:Integer );
+begin
+     _DegN := DegN_;  _OnChange.Run( Self );
+end;
+
+//////////////////////////////////////////////////////////////////// M E T H O D
+
+function TCurveLanczos<_TPoin_>.Lanczos( const X_:Double ) :Double;
+begin
+     if Abs( X_ ) < _DegN then Result := Sinc( X_ ) * Sinc( X_ / _DegN )
+                          else Result := 0;
+end;
+
+//     -2-t  -1-t    -t     1-t   2-t   3-t
+//        |     |     | 0   |     |     |
+//    |-----+-----+-----|-----+-----+-----|    :Basis Function
+//  +-----+-----+-----+=====+-----+-----+-----+ :Poins
+// -3    -2    -1     0     1     2     3     4
+
+function TCurveLanczos<_TPoin_>.Algorithm0( const i:Integer; const t:Double ) :_TPoin_;
+var
+   Ps :TArray<TWector>;
+   J :Integer;
+begin
+     SetLength( Ps, 2*DegN );
+
+     for J := 1-DegN to DegN do Ps[ J+DegN-1 ] := TWector.Create( Poins[ i+J ], Lanczos( J-t ) );
+
+     Result := Bary.Center( Ps );
+end;
+
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
+
+constructor TCurveLanczos<_TPoin_>.Create;
+begin
+     inherited;
+
+     AlgosN := 1;
+     _Algos[0] := Algorithm0;
+
+     DegN := 2;
+end;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+function Sinc( const X_:Single ) :Single;
+var
+   X :Single;
+begin
+     X := Pi * X_;
+     if Abs( X ) < SINGLE_EPS3 then Result := 1
+                               else Result := Sin( X ) / X;
+end;
+
+function Sinc( const X_:Double ) :Double;
+var
+   X :Double;
+begin
+     X := Pi * X_;
+     if Abs( X ) < DOUBLE_EPS3 then Result := 1
+                               else Result := Sin( X ) / X;
+end;
+
+end. //######################################################################### ■
