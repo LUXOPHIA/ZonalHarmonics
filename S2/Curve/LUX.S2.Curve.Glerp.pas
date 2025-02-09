07fa57d7436fc1785861df12e910eb26bb0a4879
luxidea <contact@luxidea.net>
2025-02-10 03:00:12 +0900

    LUX.S2.Curve ライブラリを追加。

---
+++ S2/Curve/LUX.S2.Curve.Glerp.pas
@@ -0,0 +1,163 @@
+﻿unit LUX.S2.Curve.Glerp;
+
+// Glerp :Gnomonic Linear Interpolation
+
+interface //#################################################################### ■
+
+uses LUX.S2;
+
+//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Glerp
+
+function Glerp( const P1_,P2_:TSingle2S ) :TSingle2S; overload;
+function Glerp( const P1_,P2_:TDouble2S ) :TDouble2S; overload;
+
+function Glerp( const P1_,P2_:TSingle2S; const T_:Single ) :TSingle2S; overload;
+function Glerp( const P1_,P2_:TDouble2S; const T_:Double ) :TDouble2S; overload;
+
+function Glerp( const P1_,P2_:TSingle2S; const W1_,W2_:Single ) :TSingle2S; overload;
+function Glerp( const P1_,P2_:TDouble2S; const W1_,W2_:Double ) :TDouble2S; overload;
+
+function Glerp( const P1_,P2_:TSingleW2S ) :TSingleW2S; overload;
+function Glerp( const P1_,P2_:TDoubleW2S ) :TDoubleW2S; overload;
+
+function Glerp( const Ps_:TArray<TSingle2S> ) :TSingle2S; overload;
+function Glerp( const Ps_:TArray<TDouble2S> ) :TDouble2S; overload;
+
+function Glerp( const Ps_:TArray<TSingleW2S> ) :TSingleW2S; overload;
+function Glerp( const Ps_:TArray<TDoubleW2S> ) :TDoubleW2S; overload;
+
+implementation //############################################################### ■
+
+uses LUX, LUX.Curve.Linear.D3;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Glerp
+
+function Glerp( const P1_,P2_:TSingle2S ) :TSingle2S;
+begin
+     Result := Lerp( P1_, P2_ ).Unitor;
+end;
+
+function Glerp( const P1_,P2_:TDouble2S ) :TDouble2S;
+begin
+     Result := Lerp( P1_, P2_ ).Unitor;
+end;
+
+//------------------------------------------------------------------------------
+
+function Glerp( const P1_,P2_:TSingle2S; const T_:Single ) :TSingle2S;
+begin
+     Result := Lerp( P1_, P2_, T_ ).Unitor;
+end;
+
+function Glerp( const P1_,P2_:TDouble2S; const T_:Double ) :TDouble2S;
+begin
+     Result := Lerp( P1_, P2_, T_ ).Unitor;
+end;
+
+//------------------------------------------------------------------------------
+
+function Glerp( const P1_,P2_:TSingle2S; const W1_,W2_:Single ) :TSingle2S;
+begin
+     Result := Lerp( P1_, P2_, W1_, W2_ ).Unitor;
+end;
+
+function Glerp( const P1_,P2_:TDouble2S; const W1_,W2_:Double ) :TDouble2S;
+begin
+     Result := Lerp( P1_, P2_, W1_, W2_ ).Unitor;
+end;
+
+//------------------------------------------------------------------------------
+
+function Glerp( const P1_,P2_:TSingleW2S ) :TSingleW2S;
+begin
+     Result.v := Glerp( P1_.v, P2_.v, P1_.w, P2_.w );
+     Result.w := P1_.w + P2_.w;
+end;
+
+function Glerp( const P1_,P2_:TDoubleW2S ) :TDoubleW2S;
+begin
+     Result.v := Glerp( P1_.v, P2_.v, P1_.w, P2_.w );
+     Result.w := P1_.w + P2_.w;
+end;
+
+//------------------------------------------------------------------------------
+
+function Glerp( const Ps_:TArray<TSingle2S> ) :TSingle2S;
+var
+   P :TSingle2S;
+begin
+     Result := 0;
+     for P in Ps_ do Result := Result + P;
+     Result := Result.Unitor;
+end;
+
+function Glerp( const Ps_:TArray<TDouble2S> ) :TDouble2S;
+var
+   P :TDouble2S;
+begin
+     Result := 0;
+     for P in Ps_ do Result := Result + P;
+     Result := Result.Unitor;
+end;
+
+//------------------------------------------------------------------------------
+
+function Glerp( const Ps_:TArray<TSingleW2S> ) :TSingleW2S;
+var
+   P :TSingleW2S;
+begin
+     Result := TSingleW2S.Create( 0, 0 );
+
+     for P in Ps_ do Result.w := Result.w + P.w;
+
+     if Abs( Result.w ) < SINGLE_EPS3 then
+     begin
+          for P in Ps_ do Result.v := Result.v + P.v;
+     end
+     else
+     begin
+          for P in Ps_ do Result.v := Result.v + P.w * P.v;
+
+          Result.v := Result.v / Result.w;
+     end;
+
+     Result := Result.Unitor;
+end;
+
+function Glerp( const Ps_:TArray<TDoubleW2S> ) :TDoubleW2S;
+var
+   P :TDoubleW2S;
+begin
+     Result := TDoubleW2S.Create( 0, 0 );
+
+     for P in Ps_ do Result.w := Result.w + P.w;
+
+     if Abs( Result.w ) < DOUBLE_EPS3 then
+     begin
+          for P in Ps_ do Result.v := Result.v + P.v;
+     end
+     else
+     begin
+          for P in Ps_ do Result.v := Result.v + P.w * P.v;
+
+          Result.v := Result.v / Result.w;
+     end;
+
+     Result := Result.Unitor;
+end;
+
+end. //######################################################################### ■
---
+++ S2/Curve/LUX.S2.Curve.PolySlerp.pas
@@ -0,0 +1,150 @@
+﻿unit LUX.S2.Curve.PolySlerp;
+
+// Slerp :Sherical Linear Interpolation
+
+interface //#################################################################### ■
+
+uses LUX,
+     LUX.S2,
+     LUX.S2.Curve;
+
+//type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+     //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Glerp
+
+function Glerp( const P1_,P2_:TSingle2Sw ) :TSingle2Sw; overload;
+function Glerp( const P1_,P2_:TDouble2Sw ) :TDouble2Sw; overload;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Slerp
+
+function Slerp( const P1_,P2_:TSingle2Sw ) :TSingle2Sw; overload;
+function Slerp( const P1_,P2_:TDouble2Sw ) :TDouble2Sw; overload;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PolySlerp1D
+
+function PolySlerp1D( Ps_:TArray<TSingle2Sw> ) :TSingle2Sw; overload;
+function PolySlerp1D( Ps_:TArray<TDouble2Sw> ) :TDouble2Sw; overload;
+
+implementation //############################################################### ■
+
+uses System.Math,
+     LUX.D3,
+     LUX.S2.Curve.Glerp;
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R E C O R D 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 C L A S S 】
+
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 R O U T I N E 】
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Glerp
+
+function Glerp( const P1_,P2_:TSingle2Sw ) :TSingle2Sw;
+begin
+     Result.v := Glerp( P1_.v, P2_.v, P1_.w, P2_.w );
+     Result.w := P1_.w + P2_.w;
+end;
+
+function Glerp( const P1_,P2_:TDouble2Sw ) :TDouble2Sw;
+begin
+     Result.v := Glerp( P1_.v, P2_.v, P1_.w, P2_.w );
+     Result.w := P1_.w + P2_.w;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Slerp
+
+function Slerp( const P1_,P2_:TSingle2Sw ) :TSingle2Sw;
+var
+   C, A, S :Single;
+begin
+     C := DotProduct( P1_.v, P2_.v );
+
+     if 1-SINGLE_EPS3 < Abs( C ) then Result := GLerp( P1_, P2_ )
+     else
+     begin
+          Result.w := P1_.w + P2_.w;
+
+          if Abs( Result.w ) < SINGLE_EPS3 then Result.v := 0
+          else
+          begin
+               A := ArcCos( C ) / Result.w;
+               S := Sqrt( 1 - Sqr( C ) );  //= Sin( A )
+
+               Result.v := ( Sin( A * P1_.w ) * P1_.v
+                           + Sin( A * P2_.w ) * P2_.v ) / S;
+          end;
+     end;
+end;
+
+function Slerp( const P1_,P2_:TDouble2Sw ) :TDouble2Sw;
+var
+   C, A, S :Double;
+begin
+     C := DotProduct( P1_.v, P2_.v );
+
+     if 1-DOUBLE_EPS3 < Abs( C ) then Result := GLerp( P1_, P2_ )
+     else
+     begin
+          Result.w := P1_.w + P2_.w;
+
+          if Abs( Result.w ) < DOUBLE_EPS3 then Result.v := 0
+          else
+          begin
+               A := ArcCos( C ) / Result.w;
+               S := Sqrt( 1 - Sqr( C ) );  //= Sin( A )
+
+               Result.v := ( Sin( A * P1_.w ) * P1_.v
+                           + Sin( A * P2_.w ) * P2_.v ) / S;
+          end;
+     end;
+end;
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% PolySlerp1D
+
+//  4A /1-+
+//        |- 4A+2B /1-+
+//  4B /2-+           |- 4A+3B+1C /1-+
+//        |- 2B+2C /2-+              |- 4A+4B+4C+4D
+//  4C /2-+           |- 1B+3C+4D /1-+
+//        |- 2C+4D /1-+
+//  4D /1-+
+
+function PolySlerp1D( Ps_:TArray<TSingle2Sw> ) :TSingle2Sw;
+var
+   N, L, I :Integer;
+begin
+     N := High( Ps_ );
+
+     for L := N-1 downto 0 do
+     begin
+          for I := 1 to L do Ps_[ I ] := Ps_[ I ] / 2;
+
+          for I := 0 to L do Ps_[ I ] := Ps_[ I ] + Ps_[ I+1 ];
+     end;
+
+     Result := Ps_[ 0 ];
+end;
+
+function PolySlerp1D( Ps_:TArray<TDouble2Sw> ) :TDouble2Sw;
+var
+   N, L, I :Integer;
+begin
+     N := High( Ps_ );
+
+     for L := N-1 downto 0 do
+     begin
+          for I := 1 to L do Ps_[ I ] := Ps_[ I ] / 2;
+
+          for I := 0 to L do Ps_[ I ] := Ps_[ I ] + Ps_[ I+1 ];
+     end;
+
+     Result := Ps_[ 0 ];
+end;
+
+end. //######################################################################### ■
